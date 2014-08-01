1a8a369a02a2b5a65a9a8a4fd1fbf729fcbbcc03
henderjon <jhenderson@capstonedigital.com>
2014-08-01 09:21:50 -0500

    updates example

--- examples/example.php
+++ examples/example.php
@@ -1,5 +1,10 @@
 <?php
 
+use \Chevron\Kernel\Controller;
+use \Chevron\Kernel\Dispatcher;
+use \Chevron\Kernel\Response;
+use \Chevron\Kernel\Router;
+
 /**
  * This is only an example of how the KERNEL can work. Other features/components
  * are excluded.
@@ -9,21 +14,23 @@
 
 // $di = Di goes here
 
-$router = new \Chevron\Kernel\Router\WebRouter;
+$router = new Router\WebRouter;
 $route  = $router->match($_SERVER["REQUEST_URI"]);
 
 // create an object to collect headers
+// $headers = new Response\Headers;
 
-$dispatcher = new \Chevron\Kernel\Dispatcher\Dispatcher($di);
+$dispatcher = new Dispatcher\Dispatcher($di);
 
 try{
-	$controller = $dispatcher->dispatch("ChevronWeb\\Controllers\\", $route);
-}catch(\Chevron\Kernel\Dispatcher\Exceptions\ControllerNotFoundException $e){
-	$route = new \Chevron\Kernel\Router\Route("ErrorController", "_404");
-	$controller = $dispatcher->dispatch("ChevronWeb\\Controllers\\", $route);
+	$controller = $dispatcher->dispatch("\\Chevron\\Kernel\\Controller\\Stock\\", $route);
+}catch( Dispatcher\Exceptions\ControllerNotFoundException $e ){
+	$route = new Router\Route("ErrorController", "_404");
+	$controller = $dispatcher->dispatch("\\Chevron\\Kernel\\Controller\\Stock\\", $route);
 }
 
 // at some point you might want to SEND headers, but only if you want to
+// $headers->eachHeader("header");
 
 // exec the controller -- returns a *callable* ... most likely a view (maybe a closure)
 $view = $controller();
