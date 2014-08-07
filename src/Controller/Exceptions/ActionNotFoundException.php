20071c395fb7cd27e276431dfad9446cb5598fd0
henderjon <jhenderson@capstonedigital.com>
2014-08-07 14:34:00 -0500

    adds Controller\Exceptions

---
+++ src/Controller/Exceptions/ActionNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Chevron\Kernel\Controller\Exceptions;
+/**
+ * thrown when a controller isn't found
+ * @package Chevron\Kernel
+ */
+class ActionNotFoundException extends ControllerException {}
---
+++ src/Controller/Exceptions/ControllerException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Chevron\Kernel\Controller\Exceptions;
+/**
+ * a basic exception, locally branded
+ * @package Chevron\Kernel
+ */
+class ControllerException extends \Exception {}
