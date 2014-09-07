0834cd5e82e44e6ed81637f290f4da43bddeb8f0
henderjon <jhenderson@capstonedigital.com>
2014-09-06 22:36:03 -0500

    cleans up Router

--- src/Router/CliRouter.php
+++ src/Router/CliRouter.php
@@ -16,49 +16,9 @@ class CliRouter extends AbstractRouter implements RouterInterface {
 	 * @return Route
 	 */
 	function match($path, array $params = []){
-		list($controller, $action) = $this->parseRequestUri($path);
+		list($controller, $action, $format, $query) = $this->parseRequest($path);
+		// $format and $query are ignored in the CLI
 		return new Route($controller, $action, null, $params);
-	}
-
-	/**
-	 * take the $_SERVER[REQUEST_URI] and parse it into a path, a file, and an extension
-	 * along with an array representing the query string. Everything after the domain
-	 * is assumed to be a Name\Space\Controller::action()
-	 *
-	 * domain.com/users/profiles/edit.html?id=15 should translate to Users\Profiles::edit()
-	 *
-	 * The router pays no mind to IF the class exists or should be called
-	 *
-	 * @param string $path A string representing the path to be parsed -- $_SERVER[REQUEST_URI]
-	 * @return array
-	 */
-	protected function parseRequestUri($path){
-		$url  = parse_url($path);
-
-		$parts = explode("/", $url["path"]);
-		$action = array_pop($parts); // methods are lowercase
-		array_walk($parts, function(&$v){ //, $k
-			$v = ucwords($v);
-		});
-		$class = implode("\\", $parts);
-
-		$controller = "";
-		if($class){
-			$controller = $class;
-		}
-
-		$method = $this->default_action;
-		// $format = $this->default_format;
-		if($action){
-			$method = $action;
-			if(($pos = strpos($action, ".")) !== false){
-				$method = substr($action, 0, $pos);
-				// $format = substr($action, ++$pos);
-			}
-		}
-
-		return [$controller, $method];
-
 	}
 
 }
--- src/Router/WebRouter.php
+++ src/Router/WebRouter.php
@@ -30,55 +30,8 @@ class WebRouter extends AbstractRouter implements RouterInterface {
 	 * @return Route
 	 */
 	function match($path){
-		list($controller, $action, $format, $parameters) = $this->parseRequestUri($path);
+		list($controller, $action, $format, $parameters) = $this->parseRequest($path);
 		return new Route($controller, $action, $format, $parameters);
-	}
-
-	/**
-	 * take the $_SERVER[REQUEST_URI] and parse it into a path, a file, and an extension
-	 * along with an array representing the query string. Everything after the domain
-	 * is assumed to be a Name\Space\Controller::action()
-	 *
-	 * domain.com/users/profiles/edit.html?id=15 should translate to Users\Profiles::edit()
-	 *
-	 * The router pays no mind to IF the class exists or should be called
-	 *
-	 * @note ^([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)(?:\.([\w]*))?(?:\?(.*))?
-	 *
-	 * @param string $path A string representing the path to be parsed -- $_SERVER[REQUEST_URI]
-	 * @return array
-	 */
-	protected function parseRequestUri($path){
-		$url  = parse_url($path);
-
-		$parts = explode("/", $url["path"]);
-		$action = array_pop($parts); // methods are lowercase
-		array_walk($parts, function(&$v){ //, $k
-			$v = ucwords($v);
-		});
-		$class = implode("\\", $parts);
-
-		$controller = "";
-		if($class){
-			$controller = $class;
-		}
-
-		$method = $this->default_action;
-		$format = $this->default_format;
-		if($action){
-			$method = $action;
-			if(($pos = strpos($action, ".")) !== false){
-				$method = substr($action, 0, $pos);
-				$format = substr($action, ++$pos);
-			}
-		}
-
-		$query = [];
-		if(isset($url["query"])){
-			parse_str($url["query"], $query);
-		}
-
-		return [$controller, $method, $format, $query];
 	}
 
 }
